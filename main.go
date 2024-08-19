88858dd610d55b2cf1ee701dd14397a01412fd24
Danny Oka <dannyoka@gmail.com>
2024-08-18 21:26:30 -0600

    removes added lines

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/dannyoka/go-mongo/src/config"
 	"github.com/dannyoka/go-mongo/src/db"
 	"github.com/dannyoka/go-mongo/src/repositories"
@@ -19,6 +17,4 @@ func main() {
 	db := db.InitDB(config, "test")
 	repositoriesContext := repositories.InitRepositories(db)
 	services.InitServices(repositoriesContext)
-	fmt.Println("Hello World")
-	fmt.Println("Hello World again")
 }
